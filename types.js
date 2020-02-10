98c6475f0b8db893143ff36150c6a4a7ed4ed0de
Nicole Dagnenica <nnardella@gmail.com>
2020-02-10 12:04:27 -0500

    Add practice code on types and why javascript is considered a dynamic language

---
+++ types.js
@@ -0,0 +1,35 @@
+// JAVASCRIPT TYPES
+
+
+//PRIMATIVES TYPES 
+/*
+String
+Number
+Boolean
+Null
+Undefined
+Symbol
+*/
+
+//AND OBJECT
+
+/* What is the difference between a dynamically typed language 
+such as Javascript differ from a static language such as Java?*/
+
+/* In Java you cannot easily redine a variable.
+String a = 'foo';
+a = 1; // Error
+*/
+
+/*Javascript is considered a dynamic language because the
+type of variables are determined at run time.
+
+let a = 'foo';
+console.log(typeof(a));
+// string
+
+a = 1;
+console.log(typeof(a));
+// number
+*/
+
